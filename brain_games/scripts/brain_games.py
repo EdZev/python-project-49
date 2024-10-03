e24777051a262ab97eee456fc50e4c79414c7916
Edker <p89028084234@gmail.com>
2024-10-03 12:34:32 +0500

    fix error

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-from brain_games import cli
+from brain_games.cli import welcome_user
 
 
 def main():
-    print(f'Hello {cli.welcome_user()}!')
+    print('Welcome to the Brain Games!')
+    welcome_user()
 
 
 if __name__ == '__main__':
